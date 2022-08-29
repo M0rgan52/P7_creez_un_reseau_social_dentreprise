7345306b9013afdf1ab1a9264d08a09c8d73f11e
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-29 12:25:03 +0200

    commit -m "Middl CORS"

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 module.exports = app;
