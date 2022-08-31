e039a6d9371eb1bd77817ab64b9e1b5ca24ea6e9
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-31 14:00:03 +0200

    commit -m "Instal et ajout bcrypt user schema"

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,19 +1,26 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-// const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+    {
     prenom: { type: String },
     nom: { type: String },
     fonction: { type: String },
     email: { type: String, require: true, unique: true, trim: true, match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"], lowercase: true },
     password: { type: String, require: true, maxLength: 1000, minLength: 6 },
-    picture: { type: String },
+    picture: { type: String, default: "./images/user-default.png" },
     likes: { type: [String] }
 },
     {
-        timestamps: true,
+    timestamps: true,
 });
+
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 userSchema.plugin(uniqueValidator);
 
