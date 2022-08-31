4bc2f7ee47e6506c35ae42883e272a9468a7d60c
Morgan Strohl <morgan.strohl@gmail.com>
2022-08-31 14:20:37 +0200

    commit -m "Créa user controller et nouvelle routes"

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
+// Routes d'authentification
 router.post("/register", authController.signUp);
+
+// Routes utilisateurs
+router.get("/", userController.getAllUsers);
 
 module.exports = router;
