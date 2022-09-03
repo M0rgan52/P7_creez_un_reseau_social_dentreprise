bc724bbdf9fba97890ce9f9c71446cbbaa7cd63c
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-03 14:36:22 +0200

    commit -m "Créa schéma post"

---
+++ backend/models/post.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+    {
+        posterId: { type: String, required: true },
+        message: { type: String, trim: true, maxlength: 1000 },
+        picture: { type: String },
+        video: { type: String },
+        likers: { type: [String], required: true },
+        comments: { type: [{ commenterId: String, commenterEmail: String, Text: String, timestamp: Number }], required: true },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+module.exports = mongoose.model("post", postSchema);
