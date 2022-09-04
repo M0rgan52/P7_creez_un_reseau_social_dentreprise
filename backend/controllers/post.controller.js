176a5b14f734344944a5554b652a7f3481d0c352
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-04 10:50:50 +0200

    commit -m "Ajout middl createpost"

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,14 +1,30 @@
+const postModel = require("../models/post.model");
 const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
-module.exports.readPost = async (req, res) => {
-    
-  
+module.exports.readPost = (req, res) => {
+    PostModel.find((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Erreur de connexion : " + err);
+    })
 };
 
-module.exports.createPost = (req, res) => {
+module.exports.createPost = async (req, res) => {
+    const newPost = new postModel({
+        posterId: req.body.posterId,
+        message: req.body.message,
+        video: req.body.video,
+        likers: [],
+        comments: [],
+    });
 
+    try {
+        const post = await newPost.save();
+        return res.status(201).json(post);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
 };
 
 module.exports.updatePost = async (req, res) => {
