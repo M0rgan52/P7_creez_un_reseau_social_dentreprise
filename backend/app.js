41a17ddcf86c3ac40baf34f9d6d9d2ef8d679aa5
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-06 17:40:27 +0200

    commit -m "Instal cors"

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { auth, requireAuth } = require("./middleware/auth.middleware");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
 
@@ -11,13 +12,7 @@ const postRoutes = require("./routes/post.routes");
 const app = express();
 
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
