9dc37ba1e5fdb9b01d9048acab039f2dc56ce6bf
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-06 15:06:58 +0200

    commit -m "Instal multer"

---
+++ backend/controllers/upload.controller.js
@@ -0,0 +1,4 @@
+
+module.exports.uploadProfil = (req, res) => {
+
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 // Routes d'authentification
 router.post("/register", authController.signUp);
@@ -12,5 +15,8 @@ router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getOneUsers);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
+
+// Routes avec images
+router.post("/upload", multer, uploadController.uploadProfil);
 
 module.exports = router;
