20b82d7f0ef359dd019afc89ec07fa2387a85aa3
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-10 09:11:57 +0200

    commit -m "Créa des routes"

--- frontend/client/src/components/Routes/index.js
+++ frontend/client/src/components/Routes/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import Home from "../../pages/Home";
+import Profil from "../../pages/Profil";
+
 
 const index = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/profil" exact component={Profil} />
+                <Redirect to="/" />
+
+            </Switch>
+        </Router>
     );
 };
 
