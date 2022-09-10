4d955f181759358ecfac4ab72c6fa20a91ccc3da
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-10 14:59:15 +0200

    commit -m "Ajout auth page signin et gestion token"

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Routes from "./components/Routes";
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
 
 const App = () => {
+  const [uid, setuid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => setuid(res.data))
+        .catch((err) => console.log("Pas de token"));
+    };
+    fetchToken();
+  }, [uid]);
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
+    </UidContext.Provider>
   );
 };
 
--- frontend/client/src/pages/Profil.js
+++ frontend/client/src/pages/Profil.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Log from "../components/log";
+import { UidContext } from "../components/AppContext";
 
 const Profil = () => {
+    const uid = useContext(UidContext);
+
     return (
         <div className="profil-page">
-            <div className="log-container">
-                <Log signin={false} signup={true} />
-                <div className="img-container">
-                    <img src="./logos/icon-left-font.svg" alt="Logo Groupomania" />
+            {uid ? (
+                <h1>UPDATE PAGE</h1>
+            ) : (
+                <div className="log-container">
+                    <Log signin={false} signup={true} />
+                    <div className="img-container">
+                        <img src="./logos/icon-left-font.svg" alt="Logo Groupomania" />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
 
     );
