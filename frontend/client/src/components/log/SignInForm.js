505d29d58b0d06cd63288f7957ee1379e0d15a56
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-12 10:48:25 +0200

    commit - "Modif url"

--- frontend/client/src/components/log/SignInForm.js
+++ frontend/client/src/components/log/SignInForm.js
@@ -12,7 +12,7 @@ const SignInForm = () => {
 
         axios({
             method: "post",
-            url: `${process.env.REACT_APP_API_URL}api/user/login`,
+            url: `${process.env.REACT_APP_API_URL}/api/user/login`,
             withCredentials: true,
             data: {
                 email,
--- frontend/client/src/components/log/SignUpForm.js
+++ frontend/client/src/components/log/SignUpForm.js
@@ -28,7 +28,7 @@ const SignUpForm = () => {
         } else {
             await axios({
                 method: "post",
-                url: `${process.env.REACT_APP_API_URL}api/user/register`,
+                url: `${process.env.REACT_APP_API_URL}/api/user/register`,
                 data: {
                     email,
                     password,
