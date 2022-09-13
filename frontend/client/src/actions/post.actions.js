7e3315dfecb9be8dd39e7208c04a656145eb29ec
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-13 17:04:57 +0200

    commit -m "Créa infinite scroll fil d'actu"

--- frontend/client/src/actions/post.actions.js
+++ frontend/client/src/actions/post.actions.js
@@ -4,42 +4,43 @@ export const GET_POSTS = "GET_POSTS";
 export const LIKE_POST = "LIKE_POST";
 export const DISLIKE_POST = "DISLIKE_POST";
 
-export const getPosts = () => {
-    return (dispatch) => {
-        return axios
-            .get(`${process.env.REACT_APP_API_URL}api/post`)
-            .then((res) => {
-                dispatch({ type: GET_POSTS, payload: res.data })
+export const getPosts = (num) => {
+  return (dispatch) => {
+    return axios
+      .get(`${process.env.REACT_APP_API_URL}api/post`)
+      .then((res) => {
+        const array = res.data.slice(0, num);
+        dispatch({ type: GET_POSTS, payload: array })
 
-            })
-            .catch((err) => console.log(err));
-    }
+      })
+      .catch((err) => console.log(err));
+  }
 };
 
 export const likePost = (postId, userId) => {
-    return (dispatch) => {
-      return axios({
-        method: "patch",
-        url: `${process.env.REACT_APP_API_URL}api/post/like-post/` + postId,
-        data: { id: userId },
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/post/like-post/` + postId,
+      data: { id: userId },
+    })
+      .then((res) => {
+        dispatch({ type: LIKE_POST, payload: { postId, userId } });
       })
-        .then((res) => {
-          dispatch({ type: LIKE_POST, payload: { postId, userId } });
-        })
-        .catch((err) => console.log(err));
-    };
+      .catch((err) => console.log(err));
   };
-  
-  export const dislikePost = (postId, userId) => {
-    return (dispatch) => {
-      return axios({
-        method: "patch",
-        url: `${process.env.REACT_APP_API_URL}api/post/dislike-post/` + postId,
-        data: { id: userId },
+};
+
+export const dislikePost = (postId, userId) => {
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/post/dislike-post/` + postId,
+      data: { id: userId },
+    })
+      .then((res) => {
+        dispatch({ type: DISLIKE_POST, payload: { postId, userId } });
       })
-        .then((res) => {
-          dispatch({ type: DISLIKE_POST, payload: { postId, userId } });
-        })
-        .catch((err) => console.log(err));
-    };
+      .catch((err) => console.log(err));
   };
+};
--- frontend/client/src/components/Thread.js
+++ frontend/client/src/components/Thread.js
@@ -7,14 +7,26 @@ import Card from './post/Card';
 
 const Thread = () => {
     const [loadPost, setLoadPost] = useState(true);
+    const [count, setCount] = useState(5);
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.postReducer);
 
+    const loadMore = () => {
+        if (window.innerHeight + document.documentElement.scrollTop + 1 > document.scrollingElement.scrollHeight) {
+            setLoadPost(true);
+
+        }
+    };
+
     useEffect(() => {
         if (loadPost) {
-            dispatch(getPosts());
-            setLoadPost(false)
+            dispatch(getPosts(count));
+            setLoadPost(false);
+            setCount(count + 5);
         }
+
+        window.addEventListener("scroll", loadMore);
+        return () => window.removeEventListener("scroll", loadMore);
         // eslint-disable-next-line
     }, [loadPost]);
 
