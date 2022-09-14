76e578c4877b8206730b749a4fe504b9f80c27f8
Morgan Strohl <morgan.strohl@gmail.com>
2022-09-14 02:11:08 +0200

    commit -m "Affichage form connexion page home"

---
+++ frontend/client/src/components/post/NewPostForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const NewPostForm = () => {
+
+
+    return (
+        <div className="home">
+
+
+        </div>
+    );
+};
+
+export default NewPostForm;
--- frontend/client/src/pages/Home.js
+++ frontend/client/src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Thread from "../components/Thread";
 import LeftNav from './../components/leftNav';
+import { UidContext } from './../components/AppContext';
+import NewPostForm from "../components/post/NewPostForm";
+import Log from "../components/log"
 
 const Home = () => {
+    const uid = useContext(UidContext);
+
     return (
         <div className="home">
             <LeftNav />
             <div className="main">
+                <div className="home-header" >
+                    {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
+                </div>
                 <Thread />
             </div>
 
